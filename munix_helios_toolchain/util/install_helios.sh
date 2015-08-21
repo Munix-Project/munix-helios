d97d94dadf8d8dbe8296aa3dcd26221f8f407a67
miguelangelo78 <santosmiguel25@gmail.com>
2015-08-21 18:44:01 +0100

    Toolchain improved
    No need for recreating the whole HDD from scratch. Now files can be
    created permanently

--- munix_helios_toolchain/util/install_helios.sh
+++ munix_helios_toolchain/util/install_helios.sh
@@ -6,11 +6,27 @@ DSTPATH=./helios-hdd
 
 DISKFILE=munix_helios_toolchain/helios.img
 DEVTABLE=munix_helios_toolchain/util/devtable
+MOUNTLOC=/mnt/helios
 SECTOR_COUNT=131072
 SECTOR_SIZE=4096
 HDDDIR=helios-hdd
 GENEXT=genext2fs
 OSNAME="Helios"
+
+# Allow choice between: creating entire disk from scratch from the folders 
+# OR: mount disk already created and copy the files THERE (and also the HDD folder)
+
+# Mount the disk now if any parameter was passed on
+if [[ $# -ne 0 ]]; then
+	if [[ -f $DISKFILE ]]; then
+		printf "Mounting and updating disk...\n"
+		sudo rm -rf $MOUNTLOC
+		sudo mkdir $MOUNTLOC
+		sudo mount -o loop $DISKFILE $MOUNTLOC
+	else
+		echo "Disk not found"
+	fi
+fi
 
 # Strip the extension from all object files and copy the files to the HDD
 FILELIST=$(find $TOOLCHAIN -name *.o | rename -v -f 's@\.o@@g' | grep -o -E " \./.+?")
@@ -23,20 +39,26 @@ for ARG in $(echo $FILELIST); do
 		echo $ARG - Installed
 		FILELISTDST=$(echo $ARG | sed "s@$TOOLCHAIN/$SRCPATH@$DSTPATH@g")
 		cp -f $ARG $FILELISTDST
-		#TODO: Also copy to mounted disk IF CHOICE SAYS SO
-
+		# Also copy to mounted disk IF CHOICE SAYS SO:
+		if [[ $# -ne 0 && -f $DISKFILE ]]; then
+			MOUNTFILEDST=$(echo $ARG | sed "s@$TOOLCHAIN/$SRCPATH@$MOUNTLOC@g")
+			sudo cp -f $ARG $MOUNTFILEDST
+		fi
 	fi
 done
 
-# TODO: Allow choice between: creating entire disk from scratch from the folders 
-# OR: mount disk already created and copy the files THERE (and also the HDD folder)
-
 # Now make the disk (HDD) image:
-BYTES_IN_GB=1073741824.0
-TOTAL=$(expr $SECTOR_SIZE \* $SECTOR_COUNT)
-TOTAL_GB=$(echo "scale=3;$TOTAL/$BYTES_IN_GB" | bc)
+if [[ $# -eq 0 || ! -f $DISKFILE ]]; then
+	BYTES_IN_GB=1073741824.0
+	TOTAL=$(expr $SECTOR_SIZE \* $SECTOR_COUNT)
+	TOTAL_GB=$(echo "scale=3;$TOTAL/$BYTES_IN_GB" | bc)
 
-printf "Generating $OSNAME disk image... (sector: size = $SECTOR_SIZE, count = $SECTOR_COUNT, total = $TOTAL bytes, ~$TOTAL_GB GB)\n"
-rm -f $DISKFILE
-$GENEXT -B $SECTOR_SIZE -d $HDDDIR -D $DEVTABLE -U -b $SECTOR_COUNT -N $SECTOR_SIZE $DISKFILE
+	printf "Generating $OSNAME disk image... (sector: size = $SECTOR_SIZE, count = $SECTOR_COUNT, total = $TOTAL bytes, ~$TOTAL_GB GB)\n"
+	rm -f $DISKFILE
+	$GENEXT -B $SECTOR_SIZE -d $HDDDIR -D $DEVTABLE -U -b $SECTOR_COUNT -N $SECTOR_SIZE $DISKFILE
+else
+	# The disk wasn't created, just mounted and updated the changed files
+	sudo umount $MOUNTLOC
+	sudo rm -rf $MOUNTLOC
+fi
 
